8b85385cea5a3dce6acd87905125af07ab8d9a9d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-09-13 23:50:27 +0530

    refactored

--- bin/nariya.js
+++ bin/nariya.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var argv = require('optimist')
     .usage('\nUsage: nariya --api [port] workerPort1 workerPort2 ..')
-    .demand(['api', '_'])
+    .demand(['api', 'name', '_'])
     .argv;
 
 var winstoon = require('winstoon');
@@ -9,5 +9,5 @@ winstoon.add(winstoon.transports.Console);
 var EventEmitter = require('events').EventEmitter;
 
 var eventBus = new EventEmitter();
-var api = require('../lib/api').load(argv._, process.cwd(), eventBus);
+var api = require('../lib/api').load(argv.name, argv._, process.cwd(), eventBus);
 var httpApi = require('../lib/httpApi').load(argv.api, api, eventBus);

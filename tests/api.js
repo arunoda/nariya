b093a16570544b7c695bfff910878aef5a4cfffb
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-09-14 23:22:26 +0530

    api tested

--- tests/api.js
+++ tests/api.js
@@ -13,7 +13,7 @@ exports.testDeployFailed = function(test) {
 	var deployer = horaa('deployer');
 
 
-	var logfile = path.normalize(process.env.HOME + '/.nariya/' + appName + '/logs/' + appName + '.log');
+	var logfile = path.normalize(process.env.HOME + '/.nariya/' + appName + '/logs/' + workerPorts[0] + '.log');
 	var d = nodemock.mock('deploy').takes(workerPorts[0], logfile, function() {}).calls(2, [{}]);
 	deployer.hijack('load', function(git, apps, ev) {
 		test.equal(git, gitpath);
@@ -24,6 +24,87 @@ exports.testDeployFailed = function(test) {
 	var a = api.load(appName, workerPorts, gitpath, eventBus);	
 	a.deploy(function(err) {
 		test.ok(err);
+		deployer.restore('load');
+		test.done();
+	});
+};
+
+exports.testDeployOnlyMaster = function(test) {
+	
+	var appName = 'testApi';
+	var workerPorts = [9090]
+	var gitpath = '/tmp/test';
+	var eventBus = new EventEmitter();
+	var deployer = horaa('deployer');
+
+
+	var logfile = path.normalize(process.env.HOME + '/.nariya/' + appName + '/logs/' + workerPorts[0] + '.log');
+	var d = nodemock.mock('deploy').takes(workerPorts[0], logfile, function() {}).calls(2, [null, 2323]);
+	deployer.hijack('load', function(git, apps, ev) {
+		test.equal(git, gitpath);
+		test.equal(apps, path.normalize(process.env.HOME + '/.nariya/' + appName +'/apps'));
+		return d;
+	});
+
+	var a = api.load(appName, workerPorts, gitpath, eventBus);	
+	a.deploy(function(err) {
+		test.ok(!err);
+		deployer.restore('load');
+		test.done();
+	});
+};
+
+exports.testDeployMasterAndWorker = function(test) {
+	
+	var appName = 'testApi';
+	var workerPorts = [9090, 9091]
+	var gitpath = '/tmp/test';
+	var eventBus = new EventEmitter();
+	var deployer = horaa('deployer');
+
+
+	var logpath = path.normalize(process.env.HOME + '/.nariya/' + appName + '/logs/');
+	var d = nodemock.mock('deploy').takes(workerPorts[0], logpath + workerPorts[0] + '.log', function() {}).calls(2, [null, 2323]);
+	d.mock('addWorker').takes(workerPorts[1], logpath + workerPorts[1] + '.log', function() {}).calls(2, [null, 2323]);
+	
+	deployer.hijack('load', function(git, apps, ev) {
+		test.equal(git, gitpath);
+		test.equal(apps, path.normalize(process.env.HOME + '/.nariya/' + appName +'/apps'));
+		return d;
+	});
+
+	var a = api.load(appName, workerPorts, gitpath, eventBus);	
+	a.deploy(function(err) {
+		test.ok(!err);
+		deployer.restore('load');
+		test.done();
+	});
+};
+
+exports.testDeployMasterAndWokerFailed = function(test) {
+	
+	var appName = 'testApi';
+	var workerPorts = [9090, 9091]
+	var gitpath = '/tmp/test';
+	var eventBus = new EventEmitter();
+	var deployer = horaa('deployer');
+
+
+	var logpath = path.normalize(process.env.HOME + '/.nariya/' + appName + '/logs/');
+	var d = nodemock.mock('deploy').takes(workerPorts[0], logpath + workerPorts[0] + '.log', function() {}).calls(2, [null, 2323]);
+	d.mock('addWorker').takes(workerPorts[1], logpath + workerPorts[1] + '.log', function() {}).calls(2, [{}]);
+	
+	deployer.hijack('load', function(git, apps, ev) {
+		test.equal(git, gitpath);
+		test.equal(apps, path.normalize(process.env.HOME + '/.nariya/' + appName +'/apps'));
+		return d;
+	});
+
+	var a = api.load(appName, workerPorts, gitpath, eventBus);	
+	a.deploy(function(err) {
+		test.ok(err);
+		test.equal(err.length, 1);
+		deployer.restore('load');
 		test.done();
 	});
 };

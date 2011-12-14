403d825f250dfd2fd8acd0061934e3779d96ed2c
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-12-14 17:41:21 +0530

    fixed bitbuket issue #7 deploy when pushed into a wrong branch

--- tests/repositories/bitbucket.js
+++ tests/repositories/bitbucket.js
@@ -13,6 +13,21 @@ exports.testCheck = function (test) {
 	test.done();
 };
 
+exports.testCheckForWrongBranch = function (test) {
+
+	var bitbucket = new BitBucketRepository();
+	var repositoryConfig = {
+		branch: 'master'
+	};
+
+	var payload = 'payload=%7B%22repository%22%3A+%7B%22website%22%3A+%22%22%2C+%22fork%22%3A+false%2C+%22name%22%3A+%22schema-executor%22%2C+%22scm%22%3A+%22git%22%2C+%22absolute_url%22%3A+%22%2Farunoda%2Fschema-executor%2F%22%2C+%22owner%22%3A+%22arunoda%22%2C+%22slug%22%3A+%22schema-executor%22%2C+%22is_private%22%3A+true%7D%2C+%22commits%22%3A+%5B%7B%22node%22%3A+%22d3ce75cab875%22%2C+%22files%22%3A+%5B%7B%22type%22%3A+%22modified%22%2C+%22file%22%3A+%22readme.md%22%7D%5D%2C+%22branch%22%3A+%22feature-inc-db-update%22%2C+%22utctimestamp%22%3A+%222011-12-14+11%3A57%3A45%2B00%3A00%22%2C+%22author%22%3A+%22arunoda%22%2C+%22timestamp%22%3A+%222011-12-14+12%3A57%3A45%22%2C+%22raw_node%22%3A+%22d3ce75cab8754716df64bde08d3645f983cf1e35%22%2C+%22parents%22%3A+%5B%22b46237fb3225%22%5D%2C+%22raw_author%22%3A+%22Arunoda+Susiripala+%3Carunoda.susiripala%40gmail.com%3E%22%2C+%22message%22%3A+%22readme+changed%5Cn%22%2C+%22size%22%3A+-1%2C+%22revision%22%3A+null%7D%5D%2C+%22canon_url%22%3A+%22https%3A%2F%2Fbitbucket.org%22%2C+%22user%22%3A+%22arunoda%22%7D';
+
+	var resp = bitbucket.checkForDeploy(repositoryConfig, payload);
+	test.ok(!resp);
+
+	test.done();
+};
+
 exports.testCheckMultiCommits = function (test) {
 
 	var bitbucket = new BitBucketRepository();
